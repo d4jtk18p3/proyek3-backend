0440f0a0a2ad9a1d55578ea9d0b12425fdadf8cd
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-04-13 22:21:45 +0700

    fix linter error

--- src/config/keycloak-config.js
+++ src/config/keycloak-config.js
@@ -1,43 +1,43 @@
 // const session = require('express-session');
 // const Keycloak = require('keycloak-connect');
 // import session from 'express-session';
-import Keycloak from "keycloak-connect";
+import Keycloak from 'keycloak-connect'
 
-let keycloak;
+let keycloak
 
-let keycloakConfig = {
+const keycloakConfig = {
   clientId: process.env.DEV_KEYCLOAK_CLIENTID,
   bearerOnly: true,
   serverUrl: process.env.DEV_KEYCLOAK_AUTH_URL,
   realm: process.env.DEV_KEYCLOAK_REALM,
   realmPublicKey:
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnEF7iaMslkjykYp3YviJmJaygc7tfTeqRJLbdHitJtGLNOsoDxJxZ10J7bAqlXvYoLm03tGmgtR8IOFGjvAWMGxlQrJ60xi3ZAs6lcXFmOB9+ik2CU5vhwFouVnG+bvkPgIs+qbFcxbBEgk8LJGUsLfGgNh104FO8OfqusG6MHtWKuddooEIGu1zxe0ZKGORtdP/VNxF5YG+5PAJzFH1uHsUkItCrAvCkkLzN2rjuT5Q1MhpaaIz4n2DCoaj7uuPhS7erXcOqGYR/6dEOOYPoK6Vw0M1tB2vQeBqjLbHayzdVgksZlDLlEWiluFwgoLkkXy2AuDSInRcTQtqXH1rtQIDAQAB",
-};
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnEF7iaMslkjykYp3YviJmJaygc7tfTeqRJLbdHitJtGLNOsoDxJxZ10J7bAqlXvYoLm03tGmgtR8IOFGjvAWMGxlQrJ60xi3ZAs6lcXFmOB9+ik2CU5vhwFouVnG+bvkPgIs+qbFcxbBEgk8LJGUsLfGgNh104FO8OfqusG6MHtWKuddooEIGu1zxe0ZKGORtdP/VNxF5YG+5PAJzFH1uHsUkItCrAvCkkLzN2rjuT5Q1MhpaaIz4n2DCoaj7uuPhS7erXcOqGYR/6dEOOYPoK6Vw0M1tB2vQeBqjLbHayzdVgksZlDLlEWiluFwgoLkkXy2AuDSInRcTQtqXH1rtQIDAQAB'
+}
 
-function initKeycloak() {
+function initKeycloak () {
   if (keycloak) {
-    console.log("Trying to init keycloak again");
-    return keycloak;
+    console.log('Trying to init keycloak again')
+    return keycloak
   } else {
-    console.log("Initilizing keycloak....");
+    console.log('Initilizing keycloak....')
     // kalo mau server manage authentication, maka harus ada session storage
     // tapi pada kasus ini, kita hanya ingin server melakukan validasi token dari FE ke Keycloak
     // jadi gaperlu ada storage untuk session (misal Redis)
 
     // let memoryStore = new session.MemoryStore();
     // let redisStore = new RedisStore({ client: redisClient});
-    keycloak = new Keycloak({}, keycloakConfig);
-    return keycloak;
+    keycloak = new Keycloak({}, keycloakConfig)
+    return keycloak
   }
 }
 
-function getKeycloak() {
+function getKeycloak () {
   if (!keycloak) {
     console.error(
-      "keycloak has not been initialized. Please called init first"
-    );
+      'keycloak has not been initialized. Please called init first'
+    )
   }
-  return keycloak;
+  return keycloak
 }
 
-export { initKeycloak, getKeycloak };
+export { initKeycloak, getKeycloak }
--- src/routes/test.js
+++ src/routes/test.js
@@ -1,14 +1,13 @@
 import express from 'express'
-import { initKeycloak, getKeycloak } from '../config/keycloak-config'
-import {anonymousController} from '../controller/test'
+import { initKeycloak } from '../config/keycloak-config'
+import { anonymousController } from '../controller/test'
 
-const router = express.Router();
-const keycloak = initKeycloak();
+const router = express.Router()
+const keycloak = initKeycloak()
 
-router.use(keycloak.middleware());
-router.use(keycloak.protect());
+router.use(keycloak.middleware())
+router.use(keycloak.protect())
 
-router.get('/anonymous', anonymousController);
+router.get('/anonymous', anonymousController)
 
-
-export default router;
+export default router
