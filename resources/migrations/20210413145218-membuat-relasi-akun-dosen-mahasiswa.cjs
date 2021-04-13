738fc9643f93730b62625a84dab73d839c5a4c7e
Dwinanda <dwinanda.alfauzan.tif418@polban.ac.id>
2021-04-13 22:25:36 +0700

    Menambahkan relasi migration akun dosen mahasiswa

---
+++ resources/migrations/20210413145218-membuat-relasi-akun-dosen-mahasiswa.cjs
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+        'Dosen',
+        'username',
+        {
+            type: Sequelize.STRING,
+            reference: {
+                model: 'Akun',
+                key: 'username',
+            },
+        }
+    )
+
+    await queryInterface.addColumn(
+        'Mahasiswa',
+        'username',
+        {
+            type: Sequelize.STRING,
+            reference: {
+                model: 'Akun',
+                key: 'username',
+            }
+        }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Dosen', 'username')
+    await queryInterface.removeColumn('Mahasiswa', 'username')
+  }
+};
