f4ddbd8db4b0e3c845b99fa563832567ad8c31e3
Dwinanda <dwinanda.alfauzan.tif418@polban.ac.id>
2021-04-14 11:12:35 +0700

    [server] Update Akun.js

--- src/models/Akun.js
+++ src/models/Akun.js
@@ -3,19 +3,19 @@ import Sequelize from 'sequelize'
 import db from '../db'
 
 const Akun = db.define('Akun', {
-    username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        primaryKey: true
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    tipe_akun: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  tipe_akun: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 })
 
 export default Akun
