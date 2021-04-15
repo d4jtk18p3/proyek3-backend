8ebe0478f32b3fa99037260474bf1b93b29a900b
fajrinaaflaha <48425814+fajrinaaflaha@users.noreply.github.com>
2021-04-15 15:38:07 +0700

    [server] fixing lint associations.js and rename file in models

--- src/associations.js
+++ src/associations.js
@@ -1,31 +1,32 @@
-const Dosen = require('./models/Dosen');
-const Mengajar = require('./models/Mengajar');
-const Memimpin = require('./models/Memimpin');
-const Mewalikan = require('./models/Mewalikan');
-const Prodi = require('./models/prodi');
-const TataUsaha = require('./models/TataUsaha');
+const Dosen = require('./models/Dosen')
+const Mengajar = require('./models/Mengajar')
+const Memimpin = require('./models/Memimpin')
+const Mewalikan = require('./models/Mewalikan')
+const Prodi = require('./models/prodi')
+const TataUsaha = require('./models/TataUsaha')
+const Akun = require('./models/Akun')
+const Kuliah = require('./models/Kuliah')
+const Kelas = require('./models/Kelas')
 
 const setAssociations = () => {
     Dosen.hasOne(Akun, {
         foreignKey: 'username'
-    });
+    })
     Dosen.belongsToMany(Prodi, {
         through: 'Memimpin'
-    });
+    })
     Dosen.belongsToMany(Kuliah, {
         through: 'Mengajar'
-    });
-
+    })
     Prodi.hasMany(Kelas, {
         foreignKey: 'kode_prodi'
-    });
-
+    })
     TataUsaha.hasOne(Akun, {
         foreignKey: 'username'
-    });
-    TataUsaha.hasMany(Presensi,{
+    })
+    TataUsaha.hasMany(Presensi, {
         foreignKey: 'NIP'
-    });
-};
+    })
+}
 
-module.exports = setAssociations;
+module.exports = setAssociations
