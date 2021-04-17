77ca3cbe580674acc811e39f7b67d517de37c27d
mufqiuwais <50223787+mufqiuwais@users.noreply.github.com>
2021-04-17 13:12:39 +0700

    [server] membuat associations mufqi (#11)
    * [server] Add migration relation and fixing model
    
    menambah kuliah_mahasiswa dan rencana_studi, membuat relasi tabel jadwal, nilai, presensi, prodi, kuliah_mahasiswa, dan rencana_studi
    
    * Revert "[server] Add migration relation and fixing model"
    
    This reverts commit 3f8811230a87739103b6669371b7154087e5a7b7.
    
    * [server] adding new tables and migration relation
    
    * [server] adding migration relation and new model
    
    * [server] adding model in src/models
    
    * [server] fixing linter
    
    * [server] fixing linter 2
    
    * [server] fixing package
    
    * [server] adding associations
    
    * [server] fixing linter associations
    
    * [server] single quote strings

---
+++ src/associations.js
@@ -0,0 +1,52 @@
+const Jadwal = require('./models/jadwal')
+const Presensi = require('./models/presensi')
+const MataKuliah = require('./models/MataKuliah')
+const Kuliah = require('./models/Kuliah')
+const Kelas = require('./models/Kelas')
+const KuliahMahasiswa = require('./models/kuliah_mahasiswa')
+const Mahasiswa = require('./models/Mahasiswa')
+const Dosen = require('./models/Dosen')
+const RencanaStudi = require('./models/rencana_studi')
+const Nilai = require('./models/nilai')
+const Prodi = require('./models/prodi')
+const TataUsaha = require('./models/TataUsaha')
+const setAssociations = () => {
+  Kuliah.belongsToMany(Mahasiswa, {
+    through: 'RencanaStudi'
+  })
+  Kuliah.hasMany(Kelas, {
+    foreignKey: 'id_kuliah'
+  })
+  Kuliah.belongsToMany(Dosen, {
+    through: 'Mengajar'
+  })
+  MataKuliah.hasMany(Kuliah, {
+    foreignKey: 'id_mata_kuliah'
+  })
+  MataKuliah.hasMany(Nilai, {
+    foreignKey: 'id_mata_kuliah'
+  })
+  Kelas.hasMany(Jadwal, {
+    foreignKey: 'id_kelas'
+  })
+  Kelas.belongsToMany(Dosen, {
+    through: 'Mewalikan'
+  })
+  Kelas.belongsTo(Prodi, {
+    foreignKey: 'kode_prodi'
+  })
+  KuliahMahasiswa.hasMany(Presensi, {
+    foreignKey: 'kuliah_mahasiswa_id'
+  })
+  RencanaStudi.belongsToMany(Kelas, {
+    through: 'KuliahMahasiswa'
+  })
+  Jadwal.hasMany(Presensi, {
+    foreignKey: 'id_jadwal'
+  })
+  Presensi.belongsTo(TataUsaha, {
+    foreignKey: 'nip'
+  })
+}
+
+module.exports = setAssociations
