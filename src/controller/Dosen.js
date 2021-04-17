740eefd5af579eb12b03b9936b3fbb33def80181
hafizmfadli <lshafizmf1st@gmail.com>
2021-04-17 17:45:50 +0700

    Menambahkan controller untuk enpoint dosen

---
+++ src/controller/Dosen.js
@@ -0,0 +1,34 @@
+import DosenDAO from '../dao/Dosen'
+
+/* 
+    Catatan:
+    1.Jangan lupa gunakan format yang telah disepakati saat mengirimkan
+      respon ke FE
+    
+    2.Kalo bikin error handling :
+    custom error example jika ingin throw error
+    const error = new Error("message nya apa");
+    error.statusCode = 4xx;
+    error.cause = `tulis cause nya apa`;
+    throw error;
+*/
+
+const postNewDosen = async(req, res, next) => {
+    try {
+      const {NIP, nama_dosen, jabatan} = req.body;
+      // console.log("BODY YANG DIKIRM ",NIP, nama_dosen, jabatan);
+      const dosen = await DosenDAO.insertOneDosen(NIP, nama_dosen, jabatan);    
+      res.status(200).json({
+        message: "insert dosen sukses",
+        data: dosen
+      });
+    } catch (error) {
+      next(error)
+    }  
+}
+
+var DosenController = {
+    postNewDosen
+}
+
+export default DosenController;
