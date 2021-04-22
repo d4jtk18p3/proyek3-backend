7a44a1488829e6607732c3e0db5c678d3df72fe9
fatharanii <48422331+fatharanii@users.noreply.github.com>
2021-04-22 21:05:11 +0700

    get all and one mahasiswa (#20)
    menambahkan fungsi
    1. get exactly one by nim
    2. get all mahasiswa
    dan fungsi search by nim dan nama

--- src/controller/Mahasiswa.js
+++ src/controller/Mahasiswa.js
@@ -49,3 +49,62 @@ export const postNewMahasiswa = async (req, res, next) => {
     next(error)
   }
 }
+
+export const getAllMahasiswa = async (req, res, next) => {
+  try {
+    const mahasiswa = await MahasiswaDAO.findAllMahasiswa()
+    res.status(200).json({
+      message: 'get all mahasiswa success',
+      data: {
+        mahasiswa
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getOneMahasiswaByNIM = async (req, res, next) => {
+  try {
+    const { NIM } = req.params
+    const mahasiswa = await MahasiswaDAO.findOneMahasiswaByNIM(NIM)
+    res.status(200).json({
+      message: 'get one Mahasiswa by NIM success',
+      data: {
+        mahasiswa
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const searchMahasiswaByName = async (req, res, next) => {
+  try {
+    const { nama } = req.params
+    const mahasiswa = await MahasiswaDAO.findMahasiswaByName(nama)
+    res.status(200).json({
+      message: 'find Mahasiswa by name success',
+      data: {
+        mahasiswa
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const searchMahasiswaByNIM = async (req, res, next) => {
+  try {
+    const { NIM } = req.params
+    const mahasiswa = await MahasiswaDAO.findMahasiswaByNIM(NIM)
+    res.status(200).json({
+      message: 'find Mahasiswa by NIM success',
+      data: {
+        mahasiswa
+      }
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- src/routes/Mahasiswa.js
+++ src/routes/Mahasiswa.js
@@ -5,5 +5,9 @@ import * as ValidatorSanitizer from '../middleware/InputValidatorSanitizer'
 const router = express.Router()
 
 router.post('/new-mahasiswa', ValidatorSanitizer.postNewMahasiswa, MahasiswaController.postNewMahasiswa)
+router.get('/AllMahasiswa', MahasiswaController.getAllMahasiswa)
+router.get('/getOne/:NIM', MahasiswaController.getOneMahasiswaByNIM)
+router.get('/searchByName/:nama', MahasiswaController.searchMahasiswaByName)
+router.get('/searchByNIM/:NIM', MahasiswaController.searchMahasiswaByNIM)
 
 export default router
