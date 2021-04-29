8bf2272e5b062948e3f1053d97d139b2e09dc4b8
mufqiuwais <mufqi.uwais.tif418@polban.ac.id>
2021-04-29 16:43:43 +0700

    [server] update migration down

--- resources/migrations/20210429083947-membuat-migration-dosen-jabatan-pengajar.cjs
+++ resources/migrations/20210429083947-membuat-migration-dosen-jabatan-pengajar.cjs
@@ -25,6 +25,6 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Pengajar');
     await queryInterface.dropTable('Jabatan');
-    await queryInterface.dropTable('Jabatan');
+    await queryInterface.dropTable('Dosen');
   }
 };
