5ce728cfb3418da80cf8d347b1b43d71a3cc1a8e
khoirunnisapk <48423384+khoirunnisapk@users.noreply.github.com>
2021-05-06 21:08:28 +0700

    [server] update association

--- src/associations.js
+++ src/associations.js
@@ -1,8 +1,10 @@
-const MataKuliah = require("./models/Mata_Kuliah");
-const Program_Studi = require("./models/Program_Studi");
+const mataKuliah = require('./models/Mata_Kuliah')
+const programStudi = require('./models/Program_Studi')
 
 const setAssociations = () => {
-    Program_Studi.hasMany(MataKuliah, {
-        foreignKey: 'kode_program_studi'
-    });
+  programStudi.hasMany(mataKuliah, {
+  foreignKey: 'kode_program_studi'
+  })
 }
+
+module.exports = setAssociations;
