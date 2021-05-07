a3ebb06ce88cee734c1eb65fff07fc7ec693110e
fajrinaaflaha <48425814+fajrinaaflaha@users.noreply.github.com>
2021-05-07 10:52:03 +0700

    Task/membuat relation fajrina (#35)
    * [server] membuat model dan migration mahasiswa-kelas
    
    * [server] fixing linter on models
    
    * [server] fixing error database migration
    
    * [server] membuat relasi mahasiswa-kelas
    
    * [server] fixing linter in associations
    
    * [server] fixing linter in associations (2)

--- src/associations.js
+++ src/associations.js
@@ -1,9 +1,21 @@
 const mataKuliah = require('./models/Mata_Kuliah')
 const programStudi = require('./models/Program_Studi')
+const mahasiswa = require('./models/Mahasiswa')
+const kelas = require('./models/Kelas')
+const perkuliahan = require('./models/Perkuliahan')
 
 const setAssociations = () => {
   programStudi.hasMany(mataKuliah, {
     foreignKey: 'kode_program_studi'
+  })
+  mahasiswa.belongsToMany(perkuliahan, {
+    through: 'Studi'
+  })
+  kelas.hasMany(mahasiswa, {
+    foreignKey: 'kode_kelas'
+  })
+  kelas.hasMany(perkuliahan, {
+    foreignKey: 'kode_kelas'
   })
 }
 
