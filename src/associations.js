5041e0fb8201f63cf2aa369d0493f6cdfec9503a
mufqiuwais <50223787+mufqiuwais@users.noreply.github.com>
2021-05-07 11:08:57 +0700

    Task/relasi dosen jabatan pengajar mufqi (#36)
    * [server] membuat model dan migration mahasiswa-kelas
    
    * [server] fixing linter on models
    
    * [server] fixing error database migration
    
    * [server] membuat relasi mahasiswa-kelas
    
    * [server] fixing linter in associations
    
    * [server] fixing linter in associations (2)
    
    * [server] membuat relasi di migration
    
    * [server] membuat associations
    
    Co-authored-by: fajrinaaflaha <48425814+fajrinaaflaha@users.noreply.github.com>
    Co-authored-by: Dwinanda <50469720+Dwinanda@users.noreply.github.com>

--- src/associations.js
+++ src/associations.js
@@ -3,6 +3,9 @@ const programStudi = require('./models/Program_Studi')
 const mahasiswa = require('./models/Mahasiswa')
 const kelas = require('./models/Kelas')
 const perkuliahan = require('./models/Perkuliahan')
+const dosen = require('./models/Dosen')
+const jabatan = require('./models/Jabatan')
+const jurusan = require('./models/Jurusan')
 
 const setAssociations = () => {
   programStudi.hasMany(mataKuliah, {
@@ -16,6 +19,24 @@ const setAssociations = () => {
   })
   kelas.hasMany(perkuliahan, {
     foreignKey: 'kode_kelas'
+  })
+  dosen.belongsToMany(perkuliahan, {
+    through: 'Pengajar'
+  })
+  perkuliahan.belongsToMany(dosen, {
+    through: 'Pengajar'
+  })
+  dosen.hasOne(kelas, {
+    foreignKey: 'nip'
+  })
+  dosen.hasMany(programStudi, {
+    foreignKey: 'nip'
+  })
+  dosen.hasMany(jurusan, {
+    foreignKey: 'nip'
+  })
+  jabatan.hasOne(dosen, {
+    foreignKey: 'id_jabatan'
   })
 }
 
