798971db7ad675e66d04acd8ee6ece3e25514b16
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-05-18 12:27:14 +0700

    add db transaction middleware

--- src/db.js
+++ src/db.js
@@ -1,5 +1,12 @@
 import Sequelize from 'sequelize'
+import { createNamespace } from 'cls-hooked'
 
-const sequelize = new Sequelize(`postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOSTNAME}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DATABASE}`, { define: { freezeTableName: true } })
+export const namespace = createNamespace('db')
+Sequelize.useCLS(namespace)
+
+const sequelize = new Sequelize(
+  `postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOSTNAME}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DATABASE}`,
+  { define: { freezeTableName: true } }
+)
 
 export default sequelize
