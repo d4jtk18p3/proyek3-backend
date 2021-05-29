b32c2bb3baf081a27d1ab3dddeed295786abda8b
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-05-29 14:47:16 +0700

    Update Pengajar.js

--- src/dao/Pengajar.js
+++ src/dao/Pengajar.js
@@ -23,7 +23,7 @@ export const getPengajarByKMTA = async (kodeMatkul, tahunAkademik) => {
     const lowerKodeMatkul = kodeMatkul.toLowerCase()
     const parsedTahunAkademik = parseInt(tahunAkademik)
     const result = await sequelize.query(
-      `SELECT "Pengajar".nip, "Pengajar".id_perkuliahan, "Pengajar"."createdAt", "Pengajar"."updatedAt", "Perkuliahan".tahun_akademik, "Perkuliahan".id_mata_kuliah, "Perkuliahan".kode_kelas, "Perkuliahan"."createdAt", "Perkuliahan"."updatedAt" FROM "Pengajar" LEFT JOIN "Perkuliahan" ON "Pengajar".id_perkuliahan = "Perkuliahan".id WHERE "Perkuliahan".tahun_akademik = ? AND LOWER("Perkuliahan".id_mata_kuliah) = ?`,
+      'SELECT "Pengajar".nip, "Pengajar".id_perkuliahan, "Pengajar"."createdAt", "Pengajar"."updatedAt", "Perkuliahan".tahun_akademik, "Perkuliahan".id_mata_kuliah, "Perkuliahan".kode_kelas, "Perkuliahan"."createdAt", "Perkuliahan"."updatedAt" FROM "Pengajar" LEFT JOIN "Perkuliahan" ON "Pengajar".id_perkuliahan = "Perkuliahan".id WHERE "Perkuliahan".tahun_akademik = ? AND LOWER("Perkuliahan".id_mata_kuliah) = ?',
       {
         replacements: [parsedTahunAkademik, lowerKodeMatkul],
         type: sequelize.QueryTypes.SELECT
