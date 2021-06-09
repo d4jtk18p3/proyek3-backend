de0225fcac4b28c116bb2111b4fa3d3a3420471f
lokajayae <lokajaya414@gmail.com>
2021-06-10 00:29:22 +0700

    Implementing admin add another admin

--- src/controller/User.js
+++ src/controller/User.js
@@ -48,6 +48,12 @@ export const createUser = async (req, res, next) => {
         error.cause = 'Insert tu ke pg gagal'
         throw error
       }
+    } else if (jenisNoInduk === '' && role === 'admin') {
+      // add admin account to database [insert code]
+      // check if insertion is failed [insert code]
+      // belum ada kejelasan mengenai admin dipegang oleh siapa
+      // admin sudah ada di sistem keycloak realm Polban-Realm dengan nama 'admin
+      // bagian ini akan ditambahkan kemudian hari
     }
 
     const tempPassword = uuid() // this is random password for keycloak
